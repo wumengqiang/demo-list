471d2c730af655111bd99fa4093b85ef90ecfe84
wumengqiang <dean@visualbusiness.com>
2017-06-20 22:01:27 +0800

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   demos/absolute-position-test/.sass-cache/b8b7b8da2029d0b4bbb9a6c9a5e1a058ddcd8c13/index.scssc
    	modified:   demos/absolute-position-test/index.html
    	new file:   demos/absolute-position-test/plugins/vue/vue@2.2.1.js
    	new file:   demos/absolute-position-test/plugins/vue/vue@2.2.1.min.js
    	modified:   demos/absolute-position-test/scripts/index.js
    	modified:   demos/absolute-position-test/scss/index.scss
    	modified:   demos/absolute-position-test/styles/index.css

--- demos/absolute-position-test/scripts/index.js
+++ demos/absolute-position-test/scripts/index.js
@@ -1,44 +1,37 @@
-var vm = new Vue({
-    el:'.vue-app',
-    data: function(){
-        return {
-            keycode: '',
-            events: [],
-            prevent: {
-                'keydown': false,
-                'compositionstart': false,
-                'compositionupdate': false,
-                'compositionend': false,
-                'input': false,
-                'keyup': false,
-                'focusin': false,
-                'focus': false,
-                'focusout': false,
-                'blur': false
+
+document.addEventListener('DOMContentLoaded', function(){
+    var vm = new Vue({
+        el:'.vue-app',
+        data: function(){
+            
+            return {
+                ep1 : getEp1Info(),
+                ep7: getEp7Info()
+            }
+        },
+        methods: {
+            record: function(type){
             }
         }
-    },
-    methods: {
-        record: function(type){
+    });
 
-            switch(type){
-                case 'keydown':
-                    this.keycode = event.keyCode;
-                    this.events = [];
-                    break;
-                case 'focus':
-                case 'blur':
-                    this.keycode = '';
-                    this.events = [];
-                    break;
-            }
-
-            if(this.prevent[type]){
-                event.preventDefault();
-                event.stopImmediatePropagation();
-            }
-
-            this.events.push(type);
-        }
+    function getEp1Info(){ // 获取example 1详情
+        var el = document.querySelector(".absolute-position"),
+            style = window.getComputedStyle(el);
+        return JSON.stringify({
+            left: style.left,
+            right: style.right,
+            top: style.top,
+            bottom: style.bottom
+        });
     }
+
+    function getEp7Info(){
+        var el = document.querySelector(".absolute-width-auto"),
+            style = window.getComputedStyle(el);
+        return  style.width;  
+    }
+
 })
+
+
